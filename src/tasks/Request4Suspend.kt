c88c5eaa59237111fdff07c99ed3e212a7b9fddc
seba <jsebas.ct@gmail.com>
2021-05-26 21:33:34 -0300

    feat: suspended sequencial without concurrency

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -2,6 +2,25 @@ package tasks
 
 import contributors.*
 
+/**
+ * In this version, while waiting for the result, we don't reuse the thread for sending other requests, because
+ *  we have written our code in a sequential way. The new request is sent only when the previous result is received.
+ * suspend functions treat the thread fairly and don't block it for "waiting",
+ * but it doesn't yet bring any concurrency to the picture.
+ * Let's see how this can be improved
+ */
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        //.execute() // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+            //.execute() // Executes request and blocks the current thread
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
 }
