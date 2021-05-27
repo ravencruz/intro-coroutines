6d7614e408ac00e92d6fc9a87f604204acbad6a9
seba <jsebas.ct@gmail.com>
2021-05-27 16:30:19 -0300

    feat: coroutines concurrently

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -12,14 +12,12 @@ import contributors.*
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
     val repos = service
         .getOrgRepos(req.org)
-        //.execute() // Executes request and blocks the current thread
         .also { logRepos(req, it) }
         .bodyList()
 
     return repos.flatMap { repo ->
         service
             .getRepoContributors(req.org, repo.name)
-            //.execute() // Executes request and blocks the current thread
             .also { logUsers(repo, it) }
             .bodyList()
     }.aggregate()
--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,20 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val def: List<Deferred<List<User>>> = repos.map { repo ->
+        async(Dispatchers.Default) {
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    def.awaitAll().flatten().aggregate()
+
 }
